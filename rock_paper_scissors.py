d16fd582b4da28c5d28cb0ec817c562a6fcb92ea
Mayur Ramesh Kyatham <kmayur632003@gmail.com>
2023-10-25 23:12:36 +0530

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,7 +2,6 @@ import random
 player1 = input("Select Rock, Paper, or Scissor :").lower()
 player2 = random.choice(["Rock", "Paper", "Scissor"]).lower()
 print("Player 2 selected: ", player2)
-
 if player1 == "rock" and player2 == "paper":
     print("Player 2 Won")
 elif player1 == "paper" and player2 == "scissor":
